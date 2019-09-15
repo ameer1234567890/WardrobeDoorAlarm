11f7261d849bc18919a837a2c492b32757050688
Ameer Dawood <ameer1234567890@gmail.com>
2019-09-15 19:55:51 +0500

    Get rid of delays and add wifi connect and OTA

--- WardrobeDoorAlarm.ino
+++ WardrobeDoorAlarm.ino
@@ -1,18 +1,61 @@
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
+#include "Secrets.h"
+
+/* Secrets.h file should contain data as below: */
+#ifndef WIFI_SSID
+#define WIFI_SSID "xxxxxxxxxx"
+#define WIFI_PASSWORD "xxxxxxxxxx"
+#endif
+
+/* Configurable variables */
 #define BUZZ_PIN 0
+#define LED_PIN 1
+#define OTA_HOSTNAME "Doorbell-Transmitter"
+#define WAIT_BEFORE_ALARM 3 // in seconds
+
+/* Do not change these unless you know what you are doing */
+int waitBeforeAlarm = WAIT_BEFORE_ALARM * 1000;
+
 
 void setup() {
   pinMode(BUZZ_PIN, OUTPUT);
   digitalWrite(BUZZ_PIN, LOW);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
   Serial.begin(115200);
   Serial.println("Startup");
-  delay(30000);
-  Serial.println("End of wait");
+  setupWifi();
+  ArduinoOTA.setHostname(OTA_HOSTNAME);
+  ArduinoOTA.begin();
 }
 
+
 void loop() {
-  tone(BUZZ_PIN, 2000, 100);
-  noTone(100);
-  delay(200);
+  if (millis() > waitBeforeAlarm) {
+    delay(waitBeforeAlarm);
+    tone(BUZZ_PIN, 2000, 100);
+    noTone(100);
+  }
+  ArduinoOTA.handle();
+}
+
+
+void setupWifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    digitalWrite(LED_PIN, HIGH);
+    delay(10);
+    digitalWrite(LED_PIN, LOW);
+    delay(10);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(700);
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
 }
